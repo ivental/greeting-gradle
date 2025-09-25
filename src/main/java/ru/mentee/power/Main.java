ab30baaecfcd07705912dd374bd36185f2ac9164
Илья Юкин <iventalll@gmail.com>
2025-09-25 13:25:46 +0300

    Initial commit: Create Gradle project

---
+++ src/main/java/ru/mentee/power/Main.java
@@ -0,0 +1,8 @@
+package ru.mentee.power;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello MP-10!");
+    }
+}
---
+++ src/test/java/ru/mentee/power/GreetingTest.java
@@ -0,0 +1,13 @@
+package ru.mentee.power;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreetingTest {
+    @Test
+    void shouldGreeting(){
+        Greeting greeting = new Greeting();
+        String result = greeting.greeting();
+        assertEquals(result, "Hello MP-10");
+
+    }
+}
